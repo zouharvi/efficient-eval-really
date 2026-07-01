62033a982f692e00b30638d16d7c3f2a3a249518
Vilém Zouhar <vilem.zouhar@gmail.com>
2026-07-01 11:09:49 +0200

    add translation and summarization

--- efficient_eval_really/data/__init__.py
+++ efficient_eval_really/data/__init__.py
@@ -3,7 +3,7 @@ from efficient_eval_really.methods import Data
 from typing import Callable
 
 def load_data_synth(
-    seed=0, models=100, items=500, heteroscedastic=False, bins=None,
+    seed=0, models=20, items=500, heteroscedastic=False, bins=None,
 ) -> Data:
     import numpy as np
 
@@ -32,16 +32,49 @@ def load_data_synth(
         data_out.append({"scores": scores_dict, "scores_metrics": scores_metric_dict, "cost": 1, "domain": "synth"})
     return data_out
 
+def load_data_subset2evaluate_translation():
+    import subset2evaluate.utils
+
+    data = subset2evaluate.utils.load_data_wmt("wmt25", "en-cs_CZ")
+    data_new = []
+    for line in data:
+        data_new.append({
+            "src": line["src"], "tgt": line["tgt"],
+            "scores": {model: line["scores"][model]["human"] for model in line["scores"]},
+            "scores_metrics": {model: {"metric": line["scores"][model]["MetricX-25"]} for model in line["scores"]},
+            "cost": 1,
+            "domain": "wmt25/en-cs",
+        })
+    return data_new
+
+def load_data_subset2evaluate_summeval():
+    import subset2evaluate.utils
+
+    data = subset2evaluate.utils.load_data_summeval(normalize=False, load_extra=True)
+    data_new = []
+    for line in data:
+        data_new.append({
+            "src": line["src"], "tgt": line["tgt"],
+            "scores": {model: line["scores"][model]["human_overall"] for model in line["scores"]},
+            "scores_metrics": {model: {"metric": line["scores"][model]["unieval_overall"]} for model in line["scores"]},
+            "cost": 1,
+            "domain": "summeval",
+        })
+    return data_new
+
 
 load_data_synth_binary : Callable[..., Data] = functools.partial(load_data_synth, bins=[0, 1])
 load_data_synth_likert : Callable[..., Data] = functools.partial(load_data_synth, bins=[0, 0.25, 0.5, 0.75, 1])
 load_data_synth_hetero : Callable[..., Data] = functools.partial(load_data_synth, heteroscedastic=True)
 load_data_synth_homo : Callable[..., Data] = functools.partial(load_data_synth, heteroscedastic=False)
 
-synth_kwargs = dict(seed=0, models=20, items=500)
+
+# TODO: some selector for src-based?
 DATA_FN = lambda: {
-    "Binary (s)": load_data_synth_binary(**synth_kwargs),
-    "Likert (s)": load_data_synth_likert(**synth_kwargs),
-    "Heterosc. (s)": load_data_synth_hetero(**synth_kwargs),
-    "Homosc. (s)": load_data_synth_homo(**synth_kwargs),
+    "Binary (s)": load_data_synth_binary(),
+    "Likert (s)": load_data_synth_likert(),
+    "Heterosc. (s)": load_data_synth_hetero(),
+    "Homosc. (s)": load_data_synth_homo(),
+    "Translation": load_data_subset2evaluate_translation(),
+    "Summarization": load_data_subset2evaluate_summeval()
 }
--- scripts/01-run_all.py
+++ scripts/01-run_all.py
@@ -9,7 +9,7 @@ import tqdm
 import argparse
 
 args = argparse.ArgumentParser()
-args.add_argument("--reuse-results", action="store_true", help="Reuse results from computed/01-results_all.json if it exists")
+args.add_argument("--reuse", action="store_true", help="Reuse results from computed/01-results_all.json if it exists")
 args.add_argument("--workers", type=int, default=5, help="Number of workers for parallelization. 0 turns off parallelization.")
 args = args.parse_args()
 
@@ -17,7 +17,7 @@ os.chdir(os.path.dirname(__file__) + "/../")
 os.makedirs("computed", exist_ok=True)
 
 # load previous results if they happen to exist
-if os.path.exists("computed/01-results_all.json") and args.reuse_results:
+if os.path.exists("computed/01-results_all.json") and args.reuse:
     with open("computed/01-results_all.json", "r") as f:
         results_out = json.load(f)
 else:
@@ -33,7 +33,7 @@ for method_name, method_fn in tqdm.tqdm(list(efficient_eval_really.methods.METHO
     results_out[method_name] = results_out.get(method_name, {})
     for data_name, data in tqdm.tqdm(DATA_FN.items()):
         # skip if already computed
-        if args.reuse_results and data_name in results_out[method_name]:
+        if args.reuse and data_name in results_out[method_name]:
             continue
 
         budgets = [int(b * len(data) * len(data[0]['scores'])) for b in BUDGETS_FRAC]
