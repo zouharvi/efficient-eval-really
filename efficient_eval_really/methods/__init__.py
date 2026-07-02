f0a4073eece30ed42033602c6c14083393a9f4a1
Niyati Bafna <niyatibafna13@gmail.com>
2026-07-02 16:47:47 -0400

    registering single non regression variant

--- efficient_eval_really/methods/__init__.py
+++ efficient_eval_really/methods/__init__.py
@@ -66,6 +66,5 @@ METHODS_BUDGETS: dict[str, Callable[[Data, Budgets], ModelScoresAtBudget]] = {
     "Anchor Points": anchor_points_budgets,
     "Tiny Benchmarks (clustering)": functools.partial(tiny_benchmarks_budgets, method="clustering"),
     "Tiny Benchmarks (IRT)": functools.partial(tiny_benchmarks_budgets, method="irt"),
-    "mRMR with Regression (kernel ridge)": functools.partial(feature_selection_regression_budgets, regressor="kernel_ridge"),
-    "mRMR with Regression (ridge)": functools.partial(feature_selection_regression_budgets, regressor="ridge"),
+    "mRMR with Regression": feature_selection_regression_budgets,
 }
